ddece9b92ef45d9318c0853d5ba507e3cded26a2
nomius1990 <nomius1990@gmail.com>
2019-06-10 12:34:01 +0800

    输入输出
    输入输出

---
+++ char_input_output.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+void funcPrint();
+void main()
+{
+    funcPrint();
+    // 向设备输出一个字符 没有换行符
+    putchar('A');
+    putchar('9');
+    putchar('\101');
+
+    puts("enter a symbol\n");
+    putchar(getchar());  //getchar 输入一个字符
+    puts("\n");
+    puts("now what you enter,then i show it\n"); //向屏幕输出一串字符
+    char chartest[10];
+    puts(gets(chartest));
+
+
+    /*
+        i,d 10进制
+        o 八进制
+        x,X 十六进制
+        u 无符号输出整形
+        c 输出字符串
+        s 输出字符串
+        f 小数类型
+        e,E 指数类型
+     */
+    printf("format outpue %d",10);
+
+}
+
+void funcPrint()
+{
+    long int longtest = 1000;
+    printf("the long is %ld \n",longtest);
+
+    /* char str1 = "LOVE"; */ 
+
+    printf("THE STRING IS %s\n","LOVE");
+    printf("THE STRING IS %10s\n","LOVE");
+    printf("THE STRING IS %-10s\n","LOVE");
+    printf("THE STRING IS %10.3s\n","LOVE"); //截取长度
+    printf("THE STRING IS %-10.3s\n","LOVE");
+}
