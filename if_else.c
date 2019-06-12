1083f5c711c5ce1796bb45761c48f91511b883d5
nomius1990 <nomius1990@gmail.com>
2019-06-12 18:00:12 +0800

    预处理-- 其他函数
    #line 100 "if_else.c"  /*将这个文件当前的行号改为100 */
    printf("2now line %d \n",__LINE__); //当前行号
    printf("filename %s \n",__FILE__); //获取当前文件名
    printf("filename createdate %d \n",__DATE__); //当前源文件的日期
    printf("filename createtime %d \n",__TIME__); //当前源文件的时间
    printf("STDC %d \n",__STDC__); //当前编译器是否为标准C 1表示符合 否则不符合

--- if_else.c
+++ if_else.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #define NUM 1
 
+#line 100 "if_else.c"  /*改变指定文件的行号 */
 void main()
 {
     int i = 0;
@@ -47,4 +48,14 @@ void main()
 #endif
     
     printf("NOW haha is %d \n",haha);
+
+    printf("1now line %d \n",__LINE__);
+    printf("2now line %d \n",__LINE__); //当前行号
+    printf("filename %s \n",__FILE__); //获取当前文件名
+    printf("filename createdate %d \n",__DATE__); //当前源文件的日期
+    printf("filename createtime %d \n",__TIME__); //当前源文件的时间
+    printf("STDC %d \n",__STDC__); //当前编译器是否为标准C 1表示符合 否则不符合
+
+
+
 }
